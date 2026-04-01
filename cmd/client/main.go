73ccc47fd830c37cdc8988c161e8b4e796e16a18
alvaro <alvaro@alvaros-MBP.fritz.box>
2026-04-01 11:17:42 +0200

    wip: client first draft

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -3,24 +3,16 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io"
 	"log"
-	"log/slog"
 	"os"
-	"path/filepath"
 
-	"github.com/alwedo/tetris/client"
+	tea "charm.land/bubbletea/v2"
+	client "github.com/alwedo/tetris/clientv2"
 )
 
 const VERSION = "v0.0.13"
 
 const (
-	hideCursor = "\033[2J\033[?25l" // also clear screen
-	showCursor = "\n\033[22;0H\n\033[?25h"
-	logFile    = ".tetrisLog"
-
-	// Option Flags.
-	debugFlag   = "debug"
 	versionFlag = "version"
 	noGhostFlag = "noghost"
 	nameFlag    = "name"
@@ -34,39 +26,16 @@ var (
 
 func main() {
 	evalOptions()
-	c, err := client.New(initLogger(), &client.Options{
-		NoGhost: noGhost,
-		Address: address,
-		Name:    name,
-	})
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Print(hideCursor)
-	defer fmt.Print(showCursor)
-	c.Start()
-}
 
-func initLogger() *slog.Logger {
-	out := io.Discard
-	if debug {
-		homeDir, err := os.UserHomeDir()
-		if err != nil {
-			log.Fatalf("error getting home directory: %v", err)
-		}
-
-		out, err = os.OpenFile(filepath.Join(homeDir, logFile), os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
-		if err != nil {
-			log.Fatalf("unable to open log file: %v", err)
-		}
+	p := tea.NewProgram(client.NewClientModel(), tea.WithWindowSize(80, 80))
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Alas, there's been an error: %v", err)
+		os.Exit(1)
 	}
-	handler := slog.NewJSONHandler(out, &slog.HandlerOptions{Level: slog.LevelDebug})
-	return slog.New(handler)
 }
 
 func evalOptions() {
 	flag.BoolFunc(versionFlag, "Prints version", version)
-	flag.BoolVar(&debug, debugFlag, false, "Enables debugging into ~/.tetrisLog")
 	flag.BoolVar(&noGhost, noGhostFlag, false, "Disables Ghost Piece")
 	flag.StringVar(&name, nameFlag, "noName", "Current player's name")
 	flag.StringVar(&address, addressFlag, "127.0.0.1", "Tetris server address")
