ade38fea8f98b1315ab32e631079fccb552d09c7
Alvaro <alvaro.alonso@cresta.ai>
2026-04-04 08:42:50 +0200

    refactor: root model

--- clientv2/root.go
+++ clientv2/root.go
@@ -2,22 +2,13 @@ package client
 
 import (
 	tea "charm.land/bubbletea/v2"
-	"github.com/alwedo/tetris/pb"
-	tetris "github.com/alwedo/tetris/tetrisv2"
 )
 
 const appName = "Terminal Tetris"
 
-// RootModel is the top-level model that orchestrates transitions between major states
 type RootModel struct {
 	currentModel tea.Model
-
-	// Panel persistence - what to show in lobby
-	lastLocalGame  *tetris.GameMessage
-	lastRemoteGame *pb.GameMessage
-
-	// Model instances (lobby is reused, games are created fresh)
-	lobbyModel *LobbyModel
+	lobbyModel   *LobbyModel
 }
 
 func NewRootModel() *RootModel {
@@ -41,41 +32,26 @@ func (m *RootModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 	switch msg := msg.(type) {
 	case TransitionToLobbyMsg:
-		return m.transitionToLobby(msg)
+		m.lobbyModel.localGameState = msg.LocalGameState
+		m.lobbyModel.remoteGameState = msg.RemoteGameState
+		m.lobbyModel.notification = msg.Message
+		m.lobbyModel.lobbyState = LobbyStateMenu
+
+		m.currentModel = m.lobbyModel
+		return m, m.currentModel.Init()
 
 	case TransitionToSingleGameMsg:
-		return m.transitionToSingleGame()
+		m.currentModel = NewSingleGameModel()
+		return m, m.currentModel.Init()
 
 	case TransitionToMPGameMsg:
-		return m.transitionToMPGame(msg)
+		m.currentModel = NewMPPlayingModel(msg.Conn, msg.Stream, msg.OpponentState)
+		return m, m.currentModel.Init()
 	}
 
 	var cmd tea.Cmd
 	m.currentModel, cmd = m.currentModel.Update(msg)
 	return m, cmd
-}
-
-func (m *RootModel) transitionToLobby(msg TransitionToLobbyMsg) (tea.Model, tea.Cmd) {
-	m.lobbyModel.localGameState = msg.LocalGameState
-	m.lobbyModel.remoteGameState = msg.RemoteGameState
-	m.lobbyModel.notification = msg.Message
-	m.lobbyModel.lobbyState = LobbyStateMenu
-
-	m.currentModel = m.lobbyModel
-	return m, m.currentModel.Init()
-}
-
-func (m *RootModel) transitionToSingleGame() (tea.Model, tea.Cmd) {
-	// Clear remote game for single player
-	m.lastRemoteGame = nil
-
-	m.currentModel = NewSingleGameModel()
-	return m, m.currentModel.Init()
-}
-
-func (m *RootModel) transitionToMPGame(msg TransitionToMPGameMsg) (tea.Model, tea.Cmd) {
-	m.currentModel = NewMPPlayingModel(msg.Conn, msg.Stream, msg.OpponentState)
-	return m, m.currentModel.Init()
 }
 
 func (m *RootModel) View() tea.View {
